a27a40327a0916af1a98732296d116c7cff7594d
Tuan Trung <tranvantuantrung@gmail.com>
2020-05-16 22:23:03 +0700

    build storybook

---
+++ .out/precache-manifest.f8952f0346c7b8ab6fc9b7d9c4ef3802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2377c414cfa74b08bbbd240d6406595",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2a9183f7916758db5468.bundle.js"
+  },
+  {
+    "url": "runtime~main.2a9183f7916758db5468.bundle.js"
+  },
+  {
+    "revision": "46c5b8611afc2bba2fcd",
+    "url": "static/css/main.c716e20e.chunk.css"
+  },
+  {
+    "revision": "8bb4bfd50411eda431c3",
+    "url": "static/css/vendors~main.aa5be0c0.chunk.css"
+  },
+  {
+    "url": "vendors~main.2a9183f7916758db5468.bundle.js"
+  },
+  {
+    "url": "vendors~main.2a9183f7916758db5468.bundle.js.LICENSE.txt"
+  }
+]);
